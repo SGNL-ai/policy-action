96563db4c665141f749f8127f699ec7e4047ad49
Erik Gustavson <erik@sgnl.ai>
2024-12-02 12:20:03 -0800

    moving eslint to flatConfig

---
+++ .github/linters/eslint.config.mjs
@@ -0,0 +1,77 @@
+import jest from 'eslint-plugin-jest'
+import globals from 'globals'
+import babelParser from '@babel/eslint-parser'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import js from '@eslint/js'
+import { FlatCompat } from '@eslint/eslintrc'
+import github from 'eslint-plugin-github'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all
+})
+
+export default [
+  github.getFlatConfigs().recommended,
+  ...compat.extends('eslint:recommended', 'plugin:jest/recommended'),
+  {
+    ignores: [
+      '!**/.*',
+      'node_modules/*',
+      'dist/*',
+      '**/*.json',
+      'coverage/*',
+      '.github/workflows/*',
+      'eslint.config.mjs'
+    ]
+  },
+  {
+    plugins: {
+      jest
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.commonjs,
+        ...globals.jest,
+        ...globals.node,
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly'
+      },
+
+      parser: babelParser,
+      ecmaVersion: 2023,
+      sourceType: 'module',
+
+      parserOptions: {
+        requireConfigFile: false,
+
+        babelOptions: {
+          babelrc: false,
+          configFile: false,
+          presets: ['jest']
+        }
+      }
+    },
+
+    rules: {
+      camelcase: 'off',
+      'eslint-comments/no-use': 'off',
+      'eslint-comments/no-unused-disable': 'off',
+      'i18n-text/no-en': 'off',
+      'import/no-commonjs': 'off',
+      'import/no-namespace': 'off',
+      'no-console': 'off',
+      'no-unused-vars': 'off',
+      semi: 'off',
+      'github/array-foreach': 'error',
+      'github/async-preventdefault': 'warn',
+      'github/no-then': 'error',
+      'github/no-blur': 'error'
+    }
+  }
+]
